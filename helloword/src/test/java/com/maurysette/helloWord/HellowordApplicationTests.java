32ba1e7c1bf1c76b2d4de80c926190984313d13b
alixia <alixia.maury@gmail.com>
2021-09-30 21:30:20 +0200

    initialisation

---
+++ helloword/src/test/java/com/maurysette/helloWord/HellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurysette.helloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
